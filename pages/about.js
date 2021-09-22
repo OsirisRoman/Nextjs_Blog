49c3a541dfd5d9a76935640ad046e84ded59b25b
Osiris Roman <osiris.roman@ulinksolution.com>
2021-09-21 23:03:53 -0500

    Added Tab Page Title with native Nextjs component

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,13 @@
+import Head from "next/head";
+
 function AboutPage() {
   return (
     <>
+      <Head>
+        <title>About - My Blog</title>
+        {/* Meta tags are useful for SEO purposes */}
+        {/* <meta name="description" value="This is the about page" /> */}
+      </Head>
       <main>
         <h1>About</h1>
       </main>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Head from "next/head";
+
 function HomePage() {
   return (
     <>
+      <Head>
+        <title>My Blog</title>
+        {/* Meta tags are useful for SEO purposes */}
+        {/* <meta name="description" value="This is my blog" /> */}
+      </Head>
       <main>
         <h1>My Blog</h1>
       </main>
